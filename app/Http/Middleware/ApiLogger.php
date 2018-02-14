a7ca18e7b791d526ccb61c016da27491c59422a9
Nathan Rogers <nrogers@clevelandart.org>
2018-02-14 14:49:53 -0500

    Removing timing for API requests

--- app/Http/Middleware/ApiLogger.php
+++ app/Http/Middleware/ApiLogger.php
@@ -7,26 +7,17 @@ use Illuminate\Http\Response;
 use Illuminate\Support\Facades\Log;
 
 class ApiLogger {
-  protected $start_time_in_ms, $end_time_in_ms;
-
-  public function handle(Request $request, Closure $next) {
-    $this->start_time_in_ms = microtime(true);
-    return $next($request);
-  }
-
   public function terminate(Request $request, Response $response) {
-    $this->end_time_in_ms = microtime(true);
     $this->log($request, $response);
   }
 
   public function log(Request $request, Response $response) {
-    $duration = $this->end_time_in_ms - $this->start_time_in_ms;
     $path = $request->path();
     $method = $request->getMethod();
     $ip = $request->getClientIp();
     $status = $response->getStatusCode();
 
-    $log = "[$ip] $status {$method} {$path} - {$duration}ms";
+    $log = "[$ip] $status {$method} {$path}";
     Log::info($log);
   }
 }
